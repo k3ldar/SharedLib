b829fac116a76c4262f4e3be24e4a8b748de7a61
Si Carter <s1cart3r@gmail.com>
2023-07-30 07:07:23 +0200

    Convert to new computer

---
+++ WindowsOnly/Classes/WindowsRunProgram.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Shared;
+
+namespace SharedLib.Win.Classes
+{
+    public sealed class WindowsRunProgram : IRunProgram
+    {
+    }
+}
---
+++ src/Interfaces/IRunProgram.cs
@@ -0,0 +1,11 @@
+﻿namespace Shared
+{
+    public interface IRunProgram
+    {
+        int Run(string programName);
+
+        int Run(string programName, string parameters);
+
+        int Run(string programName, string parameters, bool useShellExecute);
+    }
+}
